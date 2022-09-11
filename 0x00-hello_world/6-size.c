5f446d10496a43cb6d6c1b527085b412cb500e92
adegadenis <adegadenis@gmail.com>
2022-09-11 07:18:25 -0700

    Prints sizeof many var

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,16 @@
 int main(void)
 
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
 
-	printf("Size of a char:%d byte(s)\n", sizeof(a));
-	printf("Size of an int:%d byte(s)\n", sizeof(b));
-	printf("Size of a long int:%d byte(s)\n", sizeof(c));
-	printf("Size of a long long int:%d byte(s)\n", sizeof(d));
-	printf("Size of a float:%d byte(s)\n", sizeof(0));
+	printf("Size of a char:%d byte(s)\n", sizeof(c));
+	printf("Size of an int:%d byte(s)\n", sizeof(i));
+	printf("Size of a long int:%d byte(s)\n", sizeof(li));
+	printf("Size of a long long int:%d byte(s)\n", sizeof(lli));
+	printf("Size of a float:%d byte(s)\n", sizeof(f));
 	return (0);
 }
