ef48decbd87b1b76a4b8feeaae21e5faab92832d
adegadenis <adegadenis@gmail.com>
2022-09-11 04:39:45 -0700

    Using printf for c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdi.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("\with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
