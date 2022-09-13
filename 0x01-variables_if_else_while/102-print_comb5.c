0e0828f6d98f6966e84290f311910df812afce30
adegadenis <adegadenis@gmail.com>
2022-09-13 10:48:46 -0700

    Prints 3 combination of numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of a pair of
+ * two digit numbers, without any repetition.
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = 0; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
