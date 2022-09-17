8b0fec7a30a932938fd97b9cb85310cc2b5c8c28
adegadenis <adegadenis@gmail.com>
2022-09-16 17:52:04 -0700

    Fwqunction that checks for uppercase character, returns 1 if yes and 0 otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * _isupper - checks if a character is uppercase
  * @c: character to be tested
  * Return: 1 whether it is, 0 otherwise
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if ((c >= 'A') && (C <= 'Z')
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
