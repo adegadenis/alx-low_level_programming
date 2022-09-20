a7495a205bf90c70249982ba6738f99b36178bd6
adegadenis <adegadenis@gmail.com>
2022-09-20 01:57:29 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,10 @@ int main(void)
 
 	i = 0;
 
-/*
- * while (1<10)
- *	{
- *		putchar(i);
- *	}
- */
+	 while (1<10)
+ 	{
+		putchar(i);
+ 	}
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
