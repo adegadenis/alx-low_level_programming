17294d283d018b5387e6a53847a21bb212c46654
adegadenis <adegadenis@gmail.com>
2022-10-25 19:30:55 -0700

    Add node at the end

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,40 @@
 #include "lists.h"
-/**
- * add_node - adds a new node at the beginning
- * of a list_t list.
- * @head: head of the linked list.
- * @str: string to store in the list.
- * Return: address of the head.
- */
 
+/**
+ * _strlen - Returns the lenght of a string.
+ * @s: Type char pointer
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	return (len);
+
+}
+
+/**
+ * *add_node - Return the number of elements in a linked list
+ * @head: data type poointer of struct node
+ * @str: data type pointer of struct
+* Return: elements of the str i
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
-	size_t n;
+	list_t *n_node;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	n_node = malloc(sizeof(list_t));
+
+	if (n_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(str);
+	n_node->str = strdup(str);
+	n_node->len = _strlen(str);
+	n_node->next = *head;
+	*head = n_node;
 
-	for (n = 0; str[n]; n++)
-		;
-
-	new_node->len = n;
-	new_node->next = *head;
-	*head = new_node;
-
-	return (*head);
+	return (n_node);
 }
