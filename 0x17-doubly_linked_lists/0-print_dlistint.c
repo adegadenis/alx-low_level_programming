5d636362c28e972d93d7bb95c65fec315c17a517
adegadenis <adegadenis@gmail.com>
2022-12-12 18:41:53 -0800

    prints all elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,15 +3,9 @@
 
 
 /**
-
- * print_dlistint - Prints all the elements of a dlistint_t list.
-
+ * print_dlistint - Prints all the elements of a dlistint_t lists
  * @h: The head of the dlistint_t list.
-
- *
-
  * Return: The number of nodes in the list.
-
  */
 
 size_t print_dlistint(const dlistint_t *h)
